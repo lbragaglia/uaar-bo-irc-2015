69a577c96a3224a01b5ddc5ab2933669b33d71b3
Lorenzo Bragaglia <lbragaglia@users.noreply.github.com>
2015-09-02 00:56:36 +0200

    Added 3 school levels. Simplified and cleaned layers.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	var resize = function() {
 		var $map = $('#map');
 
-		$map.height($(window).height() - $('div.navbar').outerHeight());
+		$map.height($(window).height() - $('nav.navbar').outerHeight(true));
 
 		if (map) {
 			map.invalidateSize();
@@ -21,18 +21,20 @@ $(document).ready(function() {
 	var osmAttrib =
 		'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
 	var baseLayer = new L.TileLayer(osmUrl, {
-		minZoom: 8,
+		minZoom: 12,
 		maxZoom: 17,
 		attribution: osmAttrib
 	});
 	baseLayer.addTo(map);
 
-	var layerControl = L.control.layers({
-		'OpenStreetMap': baseLayer
+	var stamenLayer = new L.StamenTileLayer('toner', {
+		detectRetina: true
 	}).addTo(map);
 
-	var marker;
-	var layer;
+	var layerControl = L.control.layers({
+		'OpenStreetMap': baseLayer,
+		'Stamen toner': stamenLayer
+	}).addTo(map);
 
 	var createLayerGroup = function(name) {
 		var layerGroup = new L.LayerGroup();
@@ -43,135 +45,127 @@ $(document).ready(function() {
 		return layerGroup;
 	};
 
-	var pcOptions = {
+	var percentProps = function(record) {
+		if (record.stud && record.non_freq) {
+			record.non_freq_perc = record.non_freq / record.stud * 100;
+		}
+		if (record.stud && record.stranieri) {
+			record.stranieri_perc = record.stranieri / record.stud * 100;
+		}
+		return record;
+	};
+
+	var tablePopupBuilder = function(layer, record) {
+		var $html = $(L.HTMLUtils.buildTable({
+			Quartiere: record.Quartiere,
+			Servizio: record.Servizio,
+			Struttura: record.Struttura,
+			IC: record['Istituzione scol'],
+			Indirizzo: record.indirizzo,
+			Studenti: record.stud ? record.stud : 'n/d',
+			'Non frequentanti': record.non_freq ? record.non_freq + ' (' + record.non_freq_perc
+				.toFixed(1) + '%)' : 'n/d',
+			Stranieri: record.stranieri ? record.stranieri + ' (' + record.stranieri_perc
+				.toFixed(1) + '%)' : 'n/d'
+		}));
+
+		layer.bindPopup($html.wrap('<div/>').parent().html(), {
+			minWidth: 400,
+			maxWidth: 400
+		});
+	};
+
+	var byServizio = function(servizio) {
+		return function(record) {
+			return record.Servizio === servizio;
+		};
+	};
+
+	var radiusFunc = new L.LinearFunction([0, 10], [500, 30], {
+		postProcess: function(value) {
+			if (value === 10) {
+				return 6;
+			}
+			return value;
+		}
+	});
+	var fillColorFunc = new L.PiecewiseFunction([
+		new L.HSLLuminosityFunction([0, 0], [1, 0]),
+		new L.HSLHueFunction([1, 0], [100, 240])
+	]);
+
+	var layerOptions = {
+		fillOpacity: 0.7,
+		opacity: 1,
+		weight: 1,
+		color: 'hsl(220,100%,25%)',
+		numberOfSides: 40,
+		dropShadow: true,
+		gradient: true
+	};
+
+	var displayOptions = {
+		'stud': {
+			displayName: 'Studenti',
+			displayText: function(value) {
+				return value ? value : 'n/d';
+			},
+			radius: radiusFunc
+		},
+		'stranieri_perc': {
+			displayName: 'Stranieri',
+			displayText: function(value) {
+				return value ? value.toFixed(1) + ' %' : 'n/d';
+			},
+			weight: new L.LinearFunction([0, 0.1], [100, 6])
+		},
+		'non_freq_perc': {
+			displayName: 'Non freq.',
+			displayText: function(value) {
+				return value ? value.toFixed(1) + ' %' : 'n/d';
+			},
+			fillColor: fillColorFunc
+		}
+	};
+
+	var options = {
 		recordsField: null,
 		latitudeField: 'lat',
 		longitudeField: 'lon',
-		layerOptions: {
-			fillOpacity: 0.9,
-			opacity: 1,
-			weight: 0.5,
-			radius: 10,
-			width: 5,
-			barThickness: 10
-		},
-		displayOptions: {
-			'stud': {
-				displayName: 'Totale studenti',
-				radius: new L.LinearFunction([0, 10], [500, 30]),
-			},
-			'stranieri': {
-				displayName: 'Stranieri',
-				//barThickness: new L.LinearFunction([0, 4], [500, 30])
-			}
-		},
-		chartOptions: {
-			'non_freq': {
-				color: 'hsl(0,100%,25%)',
-				fillColor: 'hsl(0,70%,60%)',
-				//maxValue: 500,
-				//maxHeight: 20,
-				displayName: 'Non freq.',
-				displayText: function(value) {
-					return value.toFixed();
-				}
-			},
-			'freq': {
-				color: 'hsl(240,100%,25%)',
-				fillColor: 'hsl(240,70%,60%)',
-				//maxValue: 500,
-				//maxHeight: 20,
-				displayName: 'Freq.',
-				displayText: function(value) {
-					return value.toFixed();
-				}
-
-			}
-		},
+		deriveProperties: percentProps,
+		layerOptions: layerOptions,
+		displayOptions: displayOptions,
 		tooltipOptions: {
-			iconSize: new L.Point(80, 55),
-			iconAnchor: new L.Point(-5, 55)
+			iconSize: new L.Point(100, 108),
+			iconAnchor: new L.Point(-5, 54)
 		},
-		onEachRecord: function(layer, record) {
-			var $html = $(L.HTMLUtils.buildTable(record));
-
-			layer.bindPopup($html.wrap('<div/>').parent().html(), {
-				minWidth: 400,
-				maxWidth: 400
-			});
-		}
+		onEachRecord: tablePopupBuilder
 	};
-	var pieChartLayer = L.pieChartDataLayer(scuoleData, pcOptions);
-	createLayerGroup('PieChart').addLayer(pieChartLayer);
 
-	var minHue = 120;
-	var maxHue = 0;
-	var rmOptions = {
-		recordsField: null,
-		latitudeField: 'lat',
-		longitudeField: 'lon',
-		deriveProperties: function(record) {
-			if (record.stud && record.non_freq) {
-				record.non_freq_perc = record.non_freq / record.stud * 100
-			}
-			return record;
-		},
-		chartOptions: {
-			'non_freq_perc': {
-				displayName: 'Non freq. (%)',
-				displayText: function(value) {
-					return value.toFixed(1);
-				},
-				color: 'hsl(240,100%,55%)',
-				fillColor: 'hsl(240,80%,55%)',
-				maxValue: 100,
-				minValue: 0
-			}
-		},
-		displayOptions: {
-			'non_freq_perc': {
-				color: new L.HSLHueFunction(new L.Point(0, minHue), new L.Point(100,
-					maxHue), {
-					outputSaturation: '100%',
-					outputLuminosity: '25%'
-				}),
-				fillColor: new L.HSLHueFunction(new L.Point(0, minHue), new L.Point(
-					100, maxHue), {
-					outputSaturation: '100%',
-					outputLuminosity: '50%'
-				})
-			}
-		},
-		layerOptions: {
-			fillOpacity: 0.9,
-			opacity: 1,
-			weight: 0.5,
-			radius: 30,
-			barThickness: 15,
-			maxDegrees: 360,
-			rotation: 0,
-			numSegments: 10
-		},
-		tooltipOptions: {
-			iconSize: new L.Point(80, 55),
-			iconAnchor: new L.Point(-5, 55)
-		},
-		onEachRecord: function(layer, record) {
-			var $html = $(L.HTMLUtils.buildTable(record));
+	var infanzia = L.extend({}, options, {
+		filter: byServizio("SCUOLA DELL'INFANZIA")
+	});
+	var infanziaLayer = L.dataLayer(scuoleData, infanzia);
+	createLayerGroup(
+		"Scuole dell'infanzia").addLayer(infanziaLayer);
 
-			layer.bindPopup($html.wrap('<div/>').parent().html(), {
-				minWidth: 400,
-				maxWidth: 400
-			});
-		}
-	};
-	var radialMeterLayer = L.radialMeterMarkerDataLayer(scuoleData, rmOptions);
-	createLayerGroup('RadialMeter').addLayer(radialMeterLayer);
+	var primaria = L.extend({}, options, {
+		filter: byServizio("SCUOLA PRIMARIA"),
+	});
+	var primariaLayer = L.dataLayer(scuoleData, primaria);
+	createLayerGroup(
+		"Scuole primarie").addLayer(primariaLayer);
 
-	$('#legend').append(pieChartLayer.getLegend({
-		numSegments: 20,
-		width: 80,
-		className: 'well',
-		//gradient: true
-	}));
+	var medieOptions = L.extend({}, options, {
+		filter: byServizio("SCUOLA SECONDARIA DI PRIMO GRADO"),
+		displayOptions: displayOptions
+	});
+	var medieLayer = new L.DataLayer(scuoleData, medieOptions);
+	createLayerGroup(
+		"Scuole secondarie di primo grado").addLayer(medieLayer);
+
+	var legendControl = L.control.legend({
+		gradient: true
+	}).addTo(map);
+	legendControl.addLayer(infanziaLayer);
 });
